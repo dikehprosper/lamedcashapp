0fd739afb48ad0c4eea1f465904a81840b6bdd0f
prosper <dikehprosper@gmail.com>
2023-11-24 15:19:45 +0100

    updated

--- src/app/firebase/firebaseConfig.ts
+++ src/app/firebase/firebaseConfig.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth } from "firebase/auth";
@@ -22,7 +21,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 const auth = getAuth();
