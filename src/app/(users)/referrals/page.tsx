d0766da158e4030c38360be6df6e1f7fc2845d6c
prosper <dikehprosper@gmail.com>
2023-12-14 20:34:00 +0100

    updated

--- src/app/(users)/referrals/page.tsx
+++ src/app/(users)/referrals/page.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import UserNav from "@/components/(Navs)/UserNav";
-const Referrals = () => {
+
+async function getAvailableCashdeskAddress(): Promise<any> {
+  const res = await fetch(
+    `/api/getAvailableCashdeskWithdrawal?timestamp=${Date.now()}`,
+    { cache: "no-store" }
+  );
+  // setCashdeskAddress(res.data.subadminWithLowestPendingCountAddress);
+  return res.json();
+  // setUser({
+  //   ...user,
+  //   cashdeskId: res.data.subadminWithLowestPendingCountId,
+  // });
+}
+const Referrals = async () => {
+  const [data] = await Promise.all([getAvailableCashdeskAddress()]);
+  console.log([data]);
   return (
     <>
       {/* <UserNav /> */}
