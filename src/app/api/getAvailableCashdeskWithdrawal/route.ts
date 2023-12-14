537ddc67f1c1f3fb34a8d6e9da167aa3d71d50ce
prosper <dikehprosper@gmail.com>
2023-12-14 19:45:31 +0100

    updated

--- src/app/api/getAvailableCashdeskWithdrawal/route.ts
+++ src/app/api/getAvailableCashdeskWithdrawal/route.ts
@@ -6,58 +6,66 @@ export async function GET(request: NextRequest) {
   try {
     // Fetch all subadmin users who handle withdrawals
 
-// Function to count "Pending" transactions for a subadmin
-const countPendingTransactions = (subadmin: any) => {
-  return subadmin.transactionHistory.filter(
-    (transaction: any) => transaction.status === "Pending"
-  ).length;
-};
+    // Function to count "Pending" transactions for a subadmin
+    const countPendingTransactions = (subadmin: any) => {
+      return subadmin.transactionHistory.filter(
+        (transaction: any) => transaction.status === "Pending"
+      ).length;
+    };
 
-const subadminUsers = await User.find({ isSubAdminWithdrawals: true });
+    const subadminUsers = await User.find({ isSubAdminWithdrawals: true });
 
-// Filter subadmin users who are not out of funds
-const subadminUsersWithFunds = subadminUsers.filter(
-  (subadminUser) => !subadminUser.isOutOfFunds
-);
+    // Filter subadmin users who are not out of funds
+    const subadminUsersWithFunds = subadminUsers.filter(
+      (subadminUser) => !subadminUser.isOutOfFunds
+    );
 
+    const sortedSubadmins = subadminUsersWithFunds.sort((a, b) => {
+      const countPendingA = countPendingTransactions(a);
+      const countPendingB = countPendingTransactions(b);
 
+      // Sort in ascending order based on the count of "pending" transactions
+      return countPendingA - countPendingB;
+    });
 
-const sortedSubadmins = subadminUsersWithFunds.sort((a, b) => {
-  const countPendingA = countPendingTransactions(a);
-  const countPendingB = countPendingTransactions(b);
+    // Log the sorted subadmins and individual counts
+    console.log("Sorted Subadmins:", sortedSubadmins);
+    sortedSubadmins.forEach((subadmin) => {
+      console.log(
+        `${subadmin.fullname}'s Pending Transaction Count:`,
+        countPendingTransactions(subadmin)
+      );
+    });
 
-  // Sort in ascending order based on the count of "pending" transactions
-  return countPendingA - countPendingB;
-});
+    const subadminWithLowestPendingCount = sortedSubadmins[0];
 
-// Log the sorted subadmins and individual counts
-console.log("Sorted Subadmins:", sortedSubadmins);
-sortedSubadmins.forEach((subadmin) => {
-  console.log(
-    `${subadmin.fullname}'s Pending Transaction Count:`,
-    countPendingTransactions(subadmin)
-  );
-});
+    // Extract necessary information
+    const subadminWithLowestPendingCountId =
+      subadminWithLowestPendingCount?._id;
+    const subadminWithLowestPendingCountAddress =
+      subadminWithLowestPendingCount?.cashdeskAddress;
 
-   const subadminWithLowestPendingCount = sortedSubadmins[0];
+    console.log(
+      "Subadmin with the lowest count id:",
+      subadminWithLowestPendingCountId
+    );
+    console.log(
+      "Subadmin with the lowest count address:",
+      subadminWithLowestPendingCountAddress
+    );
 
-// Extract necessary information
-const subadminWithLowestPendingCountId = subadminWithLowestPendingCount?._id;
-const subadminWithLowestPendingCountAddress = subadminWithLowestPendingCount?.cashdeskAddress;
+    const response = NextResponse.json({
+      message: "successful",
+      success: true,
+      subadminWithLowestPendingCountId,
+      subadminWithLowestPendingCountAddress,
+    });
 
-console.log("Subadmin with the lowest count id:", subadminWithLowestPendingCountId);
-console.log("Subadmin with the lowest count address:", subadminWithLowestPendingCountAddress);
+    // Set Cache-Control header to prevent client-side caching
+    response.headers.set("Cache-Control", "no-store");
 
-
-const response = NextResponse.json({
-  message: "successful",
-  success: true,
-  subadminWithLowestPendingCountId,
-  subadminWithLowestPendingCountAddress,
-});
-
-      // Return the response or use it as needed
-      return response;
+    // Return the response or use it as needed
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
