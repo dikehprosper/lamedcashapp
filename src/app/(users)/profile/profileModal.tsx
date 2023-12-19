8ecdebc2342ef662fc10573b302a986c067ba113
Dike Prosper Chukwuemeka <103826657+dikehprosper@users.noreply.github.com>
2023-12-19 02:05:12 +0100

    Update profileModal.tsx

--- src/app/(users)/profile/profileModal.tsx
+++ src/app/(users)/profile/profileModal.tsx
@@ -5,15 +5,17 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { FaCircle } from "react-icons/fa";
 import "./profile.css";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 
 
-export default function BasicModal({savedID}: any) {
+export default function BasicModal({savedID, betId, makeDefaultId, deleteId }: any) {
   const [open, setOpen] = React.useState(false);
   const [selectedID, setSelectedID] = React.useState<number | null>(null);
   const [defaultID, setDefaultID] = React.useState<number | null>(
-    savedID[0] || null
+    savedID[0]
   );
+  console.log(savedID[0], betId)
 
   const handleOpen = (id: number) => {
     setOpen(true);
@@ -31,27 +33,18 @@ export default function BasicModal({savedID}: any) {
     setSelectedID(null);
   };
 
-  const makeDefault = () => {
-    if (selectedID !== null) {
-      // Update the main data's savedID array
-      savedID = [selectedID, ...savedID.filter((id: any) => id !== selectedID)];
-      setDefaultID(selectedID);
-      handleClose();
-    }
-  };
 
-  const deleteID = () => {
-    if (selectedID !== null) {
-      // If the deleted ID was the default, update the default ID
-      if (selectedID === defaultID) {
-        setDefaultID(savedID[0] || null);
-      }
+   
+  const makeDefault = (id: any) => {
+    makeDefaultId(id)
+     handleClose();
+   }
+ 
 
-      // Update the main data's savedID array
-      savedID = savedID.filter((id: any) => id !== selectedID);
+  const deleteSpecificId = (id: any) => {
+    deleteId(id)
+    handleClose();
 
-      handleClose();
-    }
   };
 
   const handleModalClick = (
@@ -69,11 +62,11 @@ export default function BasicModal({savedID}: any) {
       style={{
         cursor: "pointer",
         borderColor:
-          id === defaultID
+        id === savedID[0]
             ? "rgba(256, 256, 256, 1)"
             : "rgba(256, 256, 256, 0.4)",
         color:
-          id === defaultID
+        id === savedID[0]
             ? "rgba(256, 256, 256, 1)"
             : "rgba(256, 256, 256, 0.4)",
       }}
@@ -81,22 +74,11 @@ export default function BasicModal({savedID}: any) {
       {id}
       <FaCircle
         color={
-          id === defaultID
+          id  === savedID[0]
             ? "rgba(256, 256, 256, 1)"
             : "rgba(256, 256, 256, 0.4)"
         }
       />
-      <div
-        style={{
-          cursor: "pointer",
-          color:
-            id === defaultID
-              ? "rgba(256, 256, 256, 1)"
-              : "rgba(256, 256, 256, 0.4)",
-        }}
-      >
-        X
-      </div>
       <span
         style={{
           fontSize: "8px",
@@ -127,8 +109,7 @@ export default function BasicModal({savedID}: any) {
               transform: "translate(-50%, -50%)",
               width: 400,
               backgroundColor: "rgba(0, 0, 0, 1)",
-              border: "2px solid #000",
-              padding: "60px",
+              padding: "20px 60px",
               zIndex: 1100,
               display: "flex",
               flexDirection: "column",
@@ -136,23 +117,25 @@ export default function BasicModal({savedID}: any) {
               alignItems: "center",
               justifyContent: "center",
               borderRadius: "9px",
+              border: ".5px solid grey"
             }}
           >
             <div
               style={{
                 width: "100%",
-                height: "50px",
+                height: "35px",
                 display: "flex",
                 justifyContent: "end",
                 fontSize: "22px",
                 color: "white",
               }}
-              onClick={handleClose}
+             
             >
               {" "}
-              X{" "}
+              <span   onClick={(e) => handleClose2(e)} style={{padding: '5px 10px', background:"rgba(128, 128, 128)", borderRadius:"5pX",  display: "flex",
+                justifyContent: "center", alignItems: 'center'}}><FaLongArrowAltLeft fontSize="30px" color='black' /></span>
             </div>
-            <h2 style={{ fontWeight: "bold", color: "white" }}>
+            <h2 style={{ fontWeight: "bold", color: "white", fontSize:"24px" }}>
               ID: &nbsp;{id}
             </h2>
             <div
@@ -166,8 +149,8 @@ export default function BasicModal({savedID}: any) {
               <Button
                 variant='contained'
                 color='primary'
-                onClick={makeDefault}
-                disabled={id === defaultID}
+                onClick={() => makeDefault(id)}
+                disabled={ selectedID === savedID[0]}
                 style={{
                   width: "140px",
                   whiteSpace: "nowrap",
@@ -179,8 +162,8 @@ export default function BasicModal({savedID}: any) {
               <Button
                 variant='contained'
                 color='secondary'
-                onClick={deleteID}
-                disabled={id === defaultID}
+                onClick={() => deleteSpecificId(id)}
+                disabled={selectedID === savedID[0]}
                 style={{
                   width: "140px",
                   whiteSpace: "nowrap",
