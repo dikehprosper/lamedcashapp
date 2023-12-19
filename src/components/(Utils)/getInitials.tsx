ec8fc578bc9530a3bacc31750ffc86f58bba5f90
Dike Prosper Chukwuemeka <103826657+dikehprosper@users.noreply.github.com>
2023-12-19 01:52:12 +0100

    Update getInitials.tsx

--- src/components/(Utils)/getInitials.tsx
+++ src/components/(Utils)/getInitials.tsx
@@ -1,6 +1,5 @@
 function GetInitials(name: any) {
   const data = name;
-  console.log(data)
   const fullName = data?.name;
   const initials = fullName?.split(" ").map((word: any) => word[0]?.toUpperCase());
   const result = initials?.join(""); // This will output "DP"
