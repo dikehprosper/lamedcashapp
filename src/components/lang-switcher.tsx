65f426b9216937b896bc47dd3ee5542722661bf0
prosper <dikehprosper@gmail.com>
2024-05-17 14:39:25 +0100

    updated

--- src/components/lang-switcher.tsx
+++ src/components/lang-switcher.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export type LanguageSwitcherProps = {
   context?: NextPageContext;
-};
+}
 
 export const LanguageSwitcher = ({ context }: LanguageSwitcherProps = {}) => {
   const { currentLanguage, switchLanguage, languageConfig } =
