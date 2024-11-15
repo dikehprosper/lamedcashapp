b9973c840d9fb0a1ca764123141e9aa84baac1ce
prosper <dikehprosper@gmail.com>
2024-11-15 13:09:00 +0100

    updated

--- components/(Utils)/domain.tsx
+++ components/(Utils)/domain.tsx
@@ -1,4 +1,4 @@
 const DOMAIN = "https://dev.api.betfundr.com";
-// const DOMAIN = "https://f86e-102-89-34-15.ngrok-free.app";
+// const DOMAIN = "https://1971-102-89-47-37.ngrok-free.app";
 const DOMAIN2 = "https://www.lamedcash.com";
 export { DOMAIN as default, DOMAIN2}
